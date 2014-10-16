7e34c928269952d10e477d24a15eec7d07f440c1
Daniel Jozie Kokott <dako@berlingskemedia.dk>
2014-10-16 09:08:01 +0200

    pooling Postgres

--- src/mdb_client.js
+++ src/mdb_client.js
@@ -3,26 +3,22 @@
 
 var pg = require('pg');
 
-//conString format "postgres://username:password@localhost/database";
-var conString = 'postgres://' + process.env.MDB_USERNAME + ':' + process.env.MDB_PASSWORD + '@' + process.env.MDB_ADDRESS + ':' + process.env.MDB_PORT + '/' + process.env.MDB_DATABASE;
-var client = {};
-
 console.log('Connecting to ' + process.env.MDB_ADDRESS +' as ' + process.env.MDB_USERNAME + '...');
 
-var client = new pg.Client(conString);
-client.connect(function(err) {
-  if (err) {
-    console.error('error fetching client from pool', err);
-    console.log('Maybe the ENV config is missing.');
-    return;
-  }
-});
+//conString format "postgres://username:password@localhost/database";
+var conString = 'postgres://' + process.env.MDB_USERNAME + ':' + process.env.MDB_PASSWORD + '@' + process.env.MDB_ADDRESS + ':' + process.env.MDB_PORT + '/' + process.env.MDB_DATABASE;
 
-module.exports = client;
-
+module.exports.query = function (sql, callback) {
+  pg.connect(conString, function (err, client, done) {
+    client.query(sql, function (err, result) {
+      done();
+      callback(err, result);
+    });
+  });
+};
 
 module.exports.queryOne = function (sql, callback) {
-  client.query(sql, function (err, result) {
+  module.exports.query(sql, function (err, result) {
     if (err) throw err;
     else if (result.rowCount === 0)
       callback(null, null);
@@ -32,5 +28,3 @@ module.exports.queryOne = function (sql, callback) {
       callback(null, result.rows[0]);
   });
 };
-
-
